6a643d713215121284085742dabf3eb3d8d5078f
kyf27@bath.ac.uk <kyf27@bath.ac.uk>
2026-01-28 21:18:39 +0000

    adding image functionality and buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
+import ImageViewer from '@/components/ImageViewer';
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+
+// The @ is a path alias for importing custom components/modules instead of relative paths
+const placeHolderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeHolderImage} />
+      </View>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>
         Go to About screen
@@ -26,5 +33,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
   },
 });
