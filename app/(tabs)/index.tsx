6ab1cc0dadada500b344c13f3072bc184079c137
kyf27@bath.ac.uk <kyf27@bath.ac.uk>
2026-01-29 23:27:22 +0000

    opening image library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
@@ -8,13 +10,30 @@ import { StyleSheet, Text, View } from "react-native";
 const placeHolderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      // launchImageLibraryAsync() receives the ImagePickerOptions object / 
+      // another object to specify different options
+      mediaTypes: ['images'],
+      allowsEditing: true, // if allowsEditing is true - user can crop the image
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={placeHolderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <Text style={styles.text}>Home screen</Text>
--- components/Button.tsx
+++ components/Button.tsx
@@ -7,9 +7,10 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 type Props = {
     label: string; // Props is an object and must have property called label. label must be a string
     theme?: 'primary'; // theme is an optional prop
+    onPress?: () => void;
 }
 
-export default function Button({ label , theme }: Props) {
+export default function Button({ label , theme, onPress }: Props) {
     // From the Props object, pull out the label property and store it in the variable called label
     // {label} is essentially {props.label}
 
@@ -22,7 +23,7 @@ export default function Button({ label , theme }: Props) {
             { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18}, ]}>
                 <Pressable
                 style={[styles.button, { backgroundColor: '#fff' }]}
-                onPress={() => alert('You pressed a button')}>
+                onPress={onPress}>
                 <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
                 <Text style={[styles.buttonLabel, { color: '#25292e'}]}>{label}</Text>
                 </Pressable>
